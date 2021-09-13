df28d16edb7c13752b78caf61ba21a8c51aab4d5
Wyatt726 <wyatt726@aol.com>
2021-09-13 18:11:54 -0400

    setting up tables for music library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const App = () => {
+  return <div className="app-container">
+    <table>
+        <thead>
+        <tr>
+          <th>title</th>
+          <th>album</th>
+          <th>artist</th>
+          <th>release date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>White House Road</td>
+          <td>Red Barn Radio</td>
+          <td>Tyler Childers</td>
+          <td>2016</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+}
+
+
 
 function App() {
   return (
